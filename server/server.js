4c67c5d3e2d42be9d9bd7f2d1e53be96e25c7a96
action_lion <49068513+kimsihyeon2@users.noreply.github.com>
2024-08-06 16:21:25 +0900

    Update CORS settings

--- server/server.js
+++ server/server.js
@@ -87,7 +87,6 @@ app.use(cors({
   ],
   credentials: true,
 }));
-
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
